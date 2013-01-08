3d7a5bcb38c15d7fc0aafba3c545de30cc5ca074
Lars Fronius <l.fronius@googlemail.com>
2013-01-08 09:35:23 +0100

    adds logfilenocry option + some more tests and parameter checking

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,10 +9,15 @@ define check_logfile (
   options = undef,
   rotation = undef,
   type = undef,
+  logfilenocry = undef,
 ) {
 
   if ! is_hash($options) {
-    fail("options must be a hash ${options}") 
+    fail("options must be a hash: '${options}'")
+  }
+
+  if ! ("$logfilenocry" in [ 'true', 'false', '' ] ) {
+    fail("logfilenocry is not a boolean or undef: '${logfilenocry}'")
   }
 
   concat::fragment { "check_logfile_${title}":
